dfb10b6b044ebace0aaaf48eaffb0b8c3ad9dfdc
mikasaukyou <43193913+mikasaukyou@users.noreply.github.com>
2024-07-17 19:16:31 +0900

    Add files via upload

---
+++ Lyra2REVc0ban.c
@@ -0,0 +1,6 @@
+#include "Lyra2RE.h"
+
+void lyra2revc0ban_hash(const char* input, char* output)
+{
+  lyra2rec0ban_hash(input, output);
+}
---
+++ Lyra2REVc0ban.h
@@ -0,0 +1,14 @@
+#ifndef LYRA2REVc0ban_H
+#define LYRA2REVc0ban_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void lyra2revc0ban_hash(const char* input, char* output);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
